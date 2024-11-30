3a3d6d9f62d1660887253b8f592303f44213eb9b
Johan Luiken <johan_luiken@hotmail.com>
2024-11-30 15:15:54 +0100

    initial commit

---
+++ Day1/DayOne.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024.Day1
+{
+    public class DayOne : IPuzzles
+    {
+        private string[] rawInput = File.ReadAllLines("day1\\input.txt");
+        public void Run()
+        {
+            var input = rawInput;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using AdventOfCode2024;
+using AdventOfCode2024.Day1;
+
+IPuzzles dayone = new DayOne();
+dayone.Run();
